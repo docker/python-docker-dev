5a6697716f6e59b7fe6839dd5408191e0e842e73
Sébastien DIDIER <73602526+sdidier-dev@users.noreply.github.com>
2023-08-24 13:58:11 +0200

    Refactor app code

--- app.py
+++ app.py
@@ -1,74 +1,47 @@
 import json
 from flask import Flask
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import os
 
 app = Flask(__name__)
 password = os.environ['POSTGRES_PASSWORD']
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
 
+
 @app.route('/widgets')
 def get_widgets():
-    conn = psycopg2.connect(
-        host="db",
-        user="postgres",
-        password=password,
-        database="example"
-    )
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM widgets")
-
-    row_headers=[x[0] for x in cursor.description]
-
-    results = cursor.fetchall()
-    json_data=[]
-    for result in results:
-        json_data.append(dict(zip(row_headers,result)))
-
-    cursor.close()
+    with psycopg2.connect(host="db", user="postgres", password=password, database="example") as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM widgets")
+            row_headers = [x[0] for x in cur.description]
+            results = cur.fetchall()
     conn.close()
 
+    json_data = [dict(zip(row_headers, result)) for result in results]
     return json.dumps(json_data)
+
 
 @app.route('/initdb')
 def db_init():
-    conn = psycopg2.connect(
-        host="db",
-        user="postgres",
-        password=password,
-    )
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-
-    cursor = conn.cursor()
-
-    cursor.execute("DROP DATABASE IF EXISTS example")
-    cursor.execute("CREATE DATABASE example")
-
-    cursor.close()
-    conn.commit()
+    conn = psycopg2.connect(host="db", user="postgres", password=password)
+    conn.set_session(autocommit=True)
+    with conn.cursor() as cur:
+        cur.execute("DROP DATABASE IF EXISTS example")
+        cur.execute("CREATE DATABASE example")
     conn.close()
 
-    conn = psycopg2.connect(
-    host="db",
-    user="postgres",
-    password=password,
-    database="example"
-    )
-    cursor = conn.cursor()
-
-    cursor.execute("DROP TABLE IF EXISTS widgets")
-    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
-
-    cursor.close()
-    conn.commit()
+    with psycopg2.connect(host="db", user="postgres", password=password, database="example") as conn:
+        with conn.cursor() as cur:
+            cur.execute("DROP TABLE IF EXISTS widgets")
+            cur.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
     conn.close()
 
     return 'init database'
 
+
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0')
+    app.run(host='0.0.0.0')
